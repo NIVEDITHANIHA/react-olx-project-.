2fb1d3fe08ee7a781a46a089807f390ef17384de
Niveditha Aravind .V <nivedha.niha@gmail.com>
2022-09-04 12:18:47 +0530

    Netflix created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
-import Weather from "./Weather App/Weather";
+import NavBar from "./components/NavBar/NavBar";
+import "./App.css";
+import Banner from "./components/Banner/Banner";
+import Rowpost from "./components/Rowpost/Rowpost";
+import { originals, action, comedy, horror } from "./urls";
+
 function App() {
   return (
     <div>
-      <Weather />
+      <NavBar />
+      <Banner />
+      <Rowpost url={originals} title="Netflix Originals" />
+      <Rowpost url={action} title="Action" isSmall />
+      <Rowpost url={comedy} title="Comedy" isSmall />
+      <Rowpost url={horror} title="Horror" isSmall />
+    
     </div>
   );
 }
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = "https://api.themoviedb.org/3";
+export const API_KEY = "57560953e0074f2065817c06592d8747";
+export const imageUrl =  "https://image.tmdb.org/t/p/original";
